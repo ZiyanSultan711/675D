454001bf29c80840808b86edc97f87cff1ddbe1e
Ziyan Sultan <ziyanssultan@gmail.com>
2020-04-10 02:23:43 -0400

    Source code push
    added odom setup code

---
+++ 2020-2021/src/main.cpp
@@ -0,0 +1,249 @@
+#include "vex.h"
+#include <vex_triport.h>
+#include <math.h>
+#include <cmath>
+using namespace vex;
+
+//globals
+competition Competition;
+brain Brain;
+
+//motors
+motor frontLeft = motor(vex::PORT1, false);
+motor backLeft = motor(vex::PORT2, false);
+motor frontRight = motor(vex::PORT3, true);
+motor backRight = motor(vex::PORT4, true);
+
+//encoders
+encoder LeftQuad(Brain.ThreeWirePort.A);
+encoder RightQuad(Brain.ThreeWirePort.C);
+encoder BackQuad(Brain.ThreeWirePort.E);
+
+/* 
+
+you need to define these variables before starting
+- wheel radius (wheelRad)
+- starting global orientation (thetaR)
+  - this is a x,y offset from a corner or something
+  - if set as 0, then local position equals global position
+- starting global position xy (dR)
+  - the angle which the robot starts at
+
+
+most important variables
+- absolute global position(d1)
+- absolute global orientation(theta1)
+
+*/
+
+//static variables
+float sL; //distance from center to left sensor wheel -- inches
+float sR; //distance from center to right sensor wheel -- inches
+float sS; //distance from center to back sensor wheel -- inches
+
+float wheelRad = 2; //radius of tracking wheel -- inches
+float wheelDiameter = wheelRad * 2; //diameter of tracking wheel -- inches
+
+float start_encoderL = LeftQuad.rotation(rotationUnits::deg);
+float start_encoderR = RightQuad.rotation(rotationUnits::deg); //starting encoder values (at last reset) -- never changed
+
+float thetaR = 0; //starting global orientation (at last reset) -- RADIAN
+
+float dR [1][2] {{0,0}}; //global position vector at last reset
+
+
+//encoder and distance variables
+float old_encoderL = 0;
+float old_encoderR = 0;
+float old_encoderS = 0; //previous encoder values -- updated at the end of ever frame
+
+float encoderL;
+float encoderR;
+float encoderS; //encoder values for each values -- updated every frame
+
+float delta_encoderL;
+float delta_encoderR;
+float delta_encoderS; // change in encoder values -- updated every frame
+
+float delta_L;
+float delta_R;
+float delta_S; // change in encoder values converted into inches -- updated every frame
+
+float total_delta_encoderL;
+float total_delta_encoderR; //total change in encoder value -- updated every frarea
+
+float delta_Lr;
+float delta_Rr; //total change in distance -- updated every frame
+
+
+//vector and displacement variables
+float d1 [1][2] {}; //absolute position
+float dO [1][2] { {dR[0][0], dR[0][1]} }; //previous global position vector
+float delta_d [1][2] {}; //global offset
+float delta_dL [1][2] {}; //local offset
+
+
+//orientation variables
+float theta1; //absolute orientation - RADIAN
+float thetaO = thetaR; //previous global orientation - RADIAN
+float thetaM; //average orientation 
+float delta_theta; //change in orintation - RADIAN
+
+
+//general odom functions
+float GetDistanceFromAngle(float Angle) //function that returns distance traveled when given change of angle
+{
+  float circumference = wheelDiameter * M_PI;
+  float Distance = circumference * (Angle/360);
+  return Distance;
+}
+
+void UpdateEncodersAndDistances() //function that updates all values and distances relating to encoders
+{
+  //updating encoder values
+  encoderL = LeftQuad.rotation(rotationUnits::deg);
+  encoderR = RightQuad.rotation(rotationUnits::deg);
+  encoderS = BackQuad.rotation(rotationUnits::deg);
+
+  //calculating change in encoders
+  delta_encoderL = encoderL - old_encoderL;
+  delta_encoderR = encoderR - old_encoderR;
+  delta_encoderS = encoderS - old_encoderS;
+
+  //updating old encoder values
+  old_encoderL = encoderL;
+  old_encoderR = encoderR;
+  old_encoderS = encoderS;
+
+  //convering change in degrees to inches
+  delta_L = GetDistanceFromAngle(delta_encoderL);
+  delta_R = GetDistanceFromAngle(delta_encoderR);
+  delta_S = GetDistanceFromAngle(delta_encoderS);
+
+  //total change in encoder values
+  total_delta_encoderL = encoderL - start_encoderL;
+  total_delta_encoderR = encoderR - start_encoderR;
+
+  //total change in distance of each wheel
+  delta_Lr = GetDistanceFromAngle(total_delta_encoderL);
+  delta_Lr = GetDistanceFromAngle(total_delta_encoderR);
+}
+
+void UpdateOdom()
+{
+  UpdateEncodersAndDistances();  //updating all encoder values and distance values
+
+  //calculate new absolute orientation
+  theta1 = thetaR + ((delta_Lr - delta_Rr)/(sL + sR));
+
+  //calculate change in orientation
+  delta_theta = theta1 - thetaO;
+
+  if(delta_theta == 0) //no change in angle
+  {
+    delta_dL[0][0] = delta_S; //x
+    delta_dL[0][1] = delta_R; //y
+  }
+  else //angle was changed
+  {
+    delta_dL[0][0] = ((delta_S/delta_theta) + sS) * 2 * sin(theta1/2);
+    delta_dL[0][1] = ((delta_R/delta_theta) + sR) * 2 * sin(theta1/2);
+  }
+
+  //calculating average orientation
+  thetaM = thetaO + (delta_theta/2);
+
+  //convert local offset to polar coordinates, 
+  float polar_r = sqrt((delta_dL[0][0] * delta_dL[0][0]) + (delta_dL[0][1] * delta_dL[0][1]));
+  float polar_theta = atan(delta_dL[0][1]/delta_dL[0][0]);
+  float polar_dL [1][2] {{polar_r, polar_theta}};
+
+  //rotate by -avg orientation (which is the global offset)
+  polar_theta = polar_theta - thetaM;
+  polar_dL[0][1] = polar_theta;
+
+  //convert back to cartiesan coordinates
+  delta_d[0][0] = polar_r * cos(polar_theta);
+  delta_d[0][1] = polar_r * sin(polar_theta);
+
+  //calculate absolute position
+  d1[0][0] = dO[0][0] + delta_d[0][0];
+  d1[0][1] = dO[0][1] + delta_d[0][1];
+
+  //update old position
+  dO[0][0] = d1[0][0];
+  dO[0][1] = d1[0][1];
+
+  //update old orientation
+  thetaO = theta1;
+}
+
+//turn functions
+void OdomPointTurnRight(float Angle) //point turn right
+{
+  //pid loop to turn right until theta1 = Angle
+}
+
+void OdomPointTurnLeft(float Angle) //point turn left
+{
+  //pid loop to turn left until theta1 = Angle
+}
+
+void OdomPointTurn(float Angle) //determine which type of point turn -- left or right
+{
+  float angleToTurnTo = theta1 - Angle;
+  angleToTurnTo = std::abs(angleToTurnTo);
+
+  if(angleToTurnTo < M_PI)
+  {
+    OdomPointTurnLeft(Angle);
+  }
+  else if(angleToTurnTo > M_PI)
+  {
+    OdomPointTurnRight(Angle);
+  }
+}
+
+void OdomPointTurnLookAt(float x2, float y2)
+{
+  float Angle = 0;
+  float x1 = d1[0][0];
+  float y1 = d1[0][1];
+
+  if(y1 > y2)
+  {
+    Angle = 180;
+  }
+
+  Angle += atan( (x2 - x1) / (y2 - y1));
+
+  OdomPointTurn(Angle);
+}
+
+
+void pre_auton(void) 
+{
+  vexcodeInit();
+}
+void autonomous(void) 
+{
+  
+}
+void usercontrol(void) 
+{
+  while (1) 
+  {
+    UpdateOdom();
+    wait(20, msec);
+  }
+}
+int main() 
+{
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+  pre_auton();
+  while (true) 
+  {
+    wait(100, msec);
+  }
+}
