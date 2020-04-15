36d8741e2cb07f0ef0a02a9845fdd7aefcbf6fbb
Ziyan Sultan <ziyanssultan@gmail.com>
2020-04-14 22:30:58 -0400

    seek point added
    just have to define the following things when calling this function
    - (x,y) of target
    - max speed of robot
    - current speed of robot
    - max force (the most the robot can turn)

--- 2020-2021/include/Odom/PathFinder.h
+++ 2020-2021/include/Odom/PathFinder.h
@@ -1,3 +1,9 @@
 
-void OdomSimplePathFinder(int numPoints, int waypoints[numPoints][2]); //simple path finder: turn to point, drive to point, repeat
-void OdomCurvePath(int numPoints, int waypoints[numPoints][2]); //given a set of points, splines will be made to turn the points into a curve
+extern void OdomSimplePathFinder(int numPoints, int waypoints[numPoints][2]); //simple path finder: turn to point, drive to point, repeat
+extern void OdomCurvePath(int numPoints, int waypoints[numPoints][2]); //given a set of points, splines will be made to turn the points into a curve
+extern void OdomSeekPoint(float x, float y, float maxSpeed, float maxForce); //seek and find a point-- this is to to be called repeatitly
+
+extern float normalizeVectorX(float vector[1][2]);
+extern float normalizeVectorY(float vector[1][2]);
+extern float getVectorMagnitude(float vector[1][2]);
+extern float perpendicularVector(float a[1][2], float b[1][2]);
--- 2020-2021/src/Odom/PathFinder.cpp
+++ 2020-2021/src/Odom/PathFinder.cpp
@@ -1,7 +1,7 @@
 #include "vex.h"
 using namespace vex;
 
-void OdomSimplePathFinder(int numPoints, int waypoints[numPoints][2]) //simple path finder: turn to point, drive to point, repeat
+void OdomSimplePathFinder(int numPoints, int waypoints[20][2]) //simple path finder: turn to point, drive to point, repeat
 {
   for(int i = 0; i > numPoints; i++) 
   {
@@ -53,4 +53,113 @@ void OdomCurvePath(int numPoints, int waypoints[20][2]) //given a set of points,
   double point = path(1.5);  this is the interpolated value at 1.5
 
   */
+}
+
+void OdomSeekPoint(float x, float y, float maxSpeed, float currentSpeed, float maxForce) //seek and find a point-- this is to to be called repeatitly
+{
+  //speeds for inside and outside wheel pairs. they will be assigned later
+  float outsideWheelSpeed = maxSpeed;
+  float insideWheelSpeed;
+
+  //speeds of left and right wheel
+  float leftSideSpeed;
+  float rightSideSpeed;
+
+  float target[1][2] = {{x,y}}; // creating 2d array with the point we are looking for
+  float location[1][2] = {{d1[0][0], d1[0][1]}}; //crearing 2d with our current point
+
+  float desired[1][2] { {target[0][0] - location[0][0], target[0][1] - location[0][1]} }; //a vector that points from roboy location to target
+
+  //normalizing the vector
+  desired[0][0] = normalizeVectorX(desired);
+  desired[0][1] = normalizeVectorY(desired);
+
+  //scaling the vector to max speed
+  desired[0][0] = desired[0][0] * maxSpeed;
+  desired[0][1] = desired[0][1] * maxSpeed;
+
+  float currentVelocity[1][2] = {{std::cos(theta1), std::cos(theta1)}}; //a velocity vector that points in the direction of movement
+
+  //normalizing the vector
+  currentVelocity[0][0] = normalizeVectorX(currentVelocity);
+  currentVelocity[0][1] = normalizeVectorY(currentVelocity);
+
+  //scaling velovity vector
+  currentVelocity[0][0] = currentVelocity[0][0] * currentSpeed;
+  currentVelocity[0][1] = currentVelocity[0][1] * currentSpeed;
+
+  float force[1][2] = { {desired[0][0] - currentVelocity[0][0], desired[0][0] - currentVelocity[0][0]} }; //creating force vector
+
+  if(getVectorMagnitude(force) > maxForce) //ensuring the force vector is limited at max force
+  {
+    force[0][0] = normalizeVectorX(force);
+    force[0][1] = normalizeVectorY(force);
+
+    force[0][0] = force[0][0] * maxForce;
+    force[0][1] = force[0][1] * maxForce;
+
+    insideWheelSpeed = maxSpeed/10;
+  }
+  else if(getVectorMagnitude(force) == 0) //if force is 0, then we are on path and we should drive at max speed
+  {
+    insideWheelSpeed = maxSpeed;
+  }
+  else //all other scenaries
+  {
+    float r = (getVectorMagnitude(force)/maxForce);
+    float speedDif = maxSpeed - (maxSpeed/10);
+    float addedSpeed = r*speedDif;
+
+    insideWheelSpeed = (maxSpeed/10) + addedSpeed;
+  }
+
+  float perpendicular = perpendicularVector(desired, currentVelocity);
+  if(perpendicular < 0)
+  {
+    rightSideSpeed = insideWheelSpeed;
+    leftSideSpeed = outsideWheelSpeed;
+  }
+  else if(perpendicular > 0)
+  {
+    rightSideSpeed = outsideWheelSpeed;
+    leftSideSpeed = insideWheelSpeed;
+  }
+  else
+  {
+    rightSideSpeed = maxSpeed;
+    leftSideSpeed = maxSpeed;
+  }
+  
+  /*
+  * we have the right side and left side speeds now
+  * now we just have to make them move :)
+  */
+
+}
+
+float normalizeVectorX(float vector[1][2])
+{
+  float mag = sqrtf( (vector[0][0] * vector[0][0]) + (vector[0][1] * vector[0][1]) );
+  vector[0][0] = vector[0][0]/mag;
+  return vector[0][0];
+}
+float normalizeVectorY(float vector[1][2])
+{
+  float mag = sqrtf( (vector[0][0] * vector[0][0]) + (vector[0][1] * vector[0][1]) );
+  vector[0][1] = vector[0][1]/mag;
+  return vector[0][1];
+}
+float getVectorMagnitude(float vector[1][2])
+{
+  float mag = sqrtf( (vector[0][0] * vector[0][0]) + (vector[0][1] * vector[0][1]) );
+  return mag;
+}
+float perpendicularVector(float a[1][2], float b[1][2])
+{
+  float ax = a[0][0];
+  float ay = a[0][1];
+  float bx = b[0][0];
+  float by = b[0][1];
+  float perpendicular = (-ax * by) + (ay * bx);
+  return perpendicular;
 }
