8b732f328c324ca921e30241be1ab61b1597d128
vedsathwik275 <63476506+vedsathwik275@users.noreply.github.com>
2021-05-22 09:33:05 -0400

    Add files via upload

---
+++ Backup Wednesday 3_24/include/PID/PIDsetup.h
@@ -0,0 +1,43 @@
+// PID CONTROLLER //
+
+// Settings
+extern double kP;
+extern double kI;
+extern double kD;
+
+extern double kPT;
+extern double kIT;
+extern double kDT;
+
+// Auton Settings
+extern int desiredVal;
+extern int turnRightDesiredVal;
+extern int turnLeftDesiredVal;
+extern int turnDesiredVal;
+
+extern int error; // Sensor Value - Desired Value  : Positional Value
+extern int prevError; // Position 10ms Ago
+extern int derivative; // Difference between Error and Previous Error  : Calculates Speed
+extern int totalError; // totalError = totalError + error
+
+extern int errorT; // Sensor Value - Desired Value  : Positional Value
+extern int prevErrorT; // Position 20ms Ago
+extern int derivativeT; // Difference between Error and Previous Error  : Calculates Speed
+extern int totalErrorT; // totalError = totalError + error
+
+// Variables Modified for Use
+extern bool enableDrivePID;
+extern bool enableTurnRightPID;
+extern bool enableTurnPID;
+extern bool resetDriveSensors;
+
+extern float driveSlow;
+extern float turnSlow;
+
+extern int drivePID();
+extern int turnPID();
+extern int pointTurn2(double direction, double degrees, double v);
+
+
+
+////////////////////
---
+++ Backup Wednesday 3_24/include/auton/helperfunctions.h
@@ -0,0 +1,9 @@
+extern void startIntake(int distance, int velocity);
+
+extern void shoot(int distance, int velocity);
+
+extern void feed(int distance, int velocity);
+
+extern void fullfeed(int distance, int velocity);
+
+extern void fastShoot(int distance, int velocity);
